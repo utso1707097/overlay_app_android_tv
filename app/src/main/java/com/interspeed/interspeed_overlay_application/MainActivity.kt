6e6714567c27ffbc6902036f3f61ee4f741c8002
utso1707097 <utso097.csekuet@gmail.com>
2024-06-09 05:11:39 -0700

    Add some functionality

--- app/src/main/java/com/interspeed/interspeed_overlay_application/MainActivity.kt
+++ app/src/main/java/com/interspeed/interspeed_overlay_application/MainActivity.kt
@@ -1,15 +1,30 @@
 package com.interspeed.interspeed_overlay_application
 
 import android.Manifest
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
+import android.animation.ValueAnimator
 import android.app.AlertDialog
+import android.app.Service
 import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.PixelFormat
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
 import android.provider.Settings
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.WindowManager
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -18,28 +33,82 @@ import androidx.core.content.ContextCompat
 class MainActivity : AppCompatActivity() {
 
     private val LOCATION_PERMISSION_REQUEST_CODE = 1
-    private val SPLASH_SCREEN_DURATION = 10000L
+    private val SPLASH_SCREEN_DURATION = 40000L
     private val ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE = 5469
+
+    private lateinit var overlayPermissionLauncher: ActivityResultLauncher<Intent>
+    private lateinit var windowManager: WindowManager
+    private lateinit var overlayView: LinearLayout
+    private lateinit var params: WindowManager.LayoutParams
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)  // Ensure this layout exists
+        setContentView(R.layout.activity_main)
+        val logoImageView: ImageView = findViewById(R.id.logoImageView)
+        animateLogo(logoImageView)
 
-        checkOverlayPermission()
+        windowManager = getSystemService(WINDOW_SERVICE) as WindowManager
+        overlayView = LayoutInflater.from(this).inflate(R.layout.overlay_layout, null) as LinearLayout
 
-        Handler().postDelayed({
-            // Check for location permissions
+        val windowType = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+        } else {
+            WindowManager.LayoutParams.TYPE_SYSTEM_ALERT
+        }
+
+        params = WindowManager.LayoutParams(
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            windowType,
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or
+                    WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL or
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS or
+                    WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON or
+                    WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN or
+                    WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM or
+                    WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH or
+                    WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED or
+                    WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
+                    WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or
+                    WindowManager.LayoutParams.FLAG_SECURE or
+                    WindowManager.LayoutParams.FLAG_LAYOUT_ATTACHED_IN_DECOR or
+                    WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR,
+            PixelFormat.TRANSLUCENT
+        )
+        params.gravity = Gravity.TOP or Gravity.CENTER_HORIZONTAL
+
+        overlayPermissionLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (Settings.canDrawOverlays(this)) {
+                windowManager.addView(overlayView, params)
+                keepOverlayOnTop()
+            }
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
                 ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // Request location permissions
                 ActivityCompat.requestPermissions(this,
                     arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
                     LOCATION_PERMISSION_REQUEST_CODE)
             } else {
                 startForegroundService()
                 openAutoStartPermissionSettings()
+                checkPermission()
             }
         }, SPLASH_SCREEN_DURATION)
+    }
+
+    private fun animateLogo(logoImageView: ImageView) {
+        val scaleX = PropertyValuesHolder.ofFloat("scaleX", 0.9f, 1.2f)
+        val scaleY = PropertyValuesHolder.ofFloat("scaleY", 0.9f, 1.2f)
+        val alpha = PropertyValuesHolder.ofFloat("alpha", 0.9f, 1.2f)
+
+        val animator = ObjectAnimator.ofPropertyValuesHolder(logoImageView, scaleX, scaleY, alpha)
+        animator.duration = 3000L
+        animator.interpolator = AccelerateDecelerateInterpolator()
+        animator.repeatMode = ValueAnimator.REVERSE
+        animator.repeatCount = ValueAnimator.INFINITE
+        animator.start()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
@@ -49,8 +118,6 @@ class MainActivity : AppCompatActivity() {
                 (grantResults.isNotEmpty() && grantResults[1] == PackageManager.PERMISSION_GRANTED)) {
                 startForegroundService()
             } else {
-                // Permissions denied, handle the error
-                // Note: You may want to provide feedback to the user that location permissions are required
                 showDialogForPermission()
             }
         }
@@ -58,11 +125,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun startForegroundService() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            startForegroundService(Intent(this, AppStartService::class.java))
+            ContextCompat.startForegroundService(this, Intent(this, OverlayService::class.java))
         } else {
-            startService(Intent(this, AppStartService::class.java))
+            startService(Intent(this, OverlayService::class.java))
         }
-        // Close the app after launching the foreground service
         finish()
     }
 
@@ -71,14 +137,12 @@ class MainActivity : AppCompatActivity() {
         builder.setTitle("Permission Required")
         builder.setMessage("Location permissions are required for the app to function properly")
         builder.setPositiveButton("Grant Permission") { dialog, _ ->
-            // Request location permissions again
             ActivityCompat.requestPermissions(this,
                 arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
                 LOCATION_PERMISSION_REQUEST_CODE)
             dialog.dismiss()
         }
         builder.setNegativeButton("Cancel") { dialog, _ ->
-            // Handle the cancellation action, such as closing the app
             finish()
             dialog.dismiss()
         }
@@ -104,11 +168,9 @@ class MainActivity : AppCompatActivity() {
                     intent.component = ComponentName("com.huawei.systemmanager", "com.huawei.systemmanager.optimize.process.ProtectActivity")
                 }
                 manufacturer.contains("sony") -> {
-                    // For Sony Android TV, you may redirect users to general settings for manual configuration
                     intent.action = Settings.ACTION_SETTINGS
                 }
                 else -> {
-                    // Handle unsupported manufacturers or direct to general settings for manual configuration
                     intent.action = Settings.ACTION_SETTINGS
                 }
             }
@@ -118,41 +180,93 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun checkOverlayPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (!Settings.canDrawOverlays(this)) {
-                val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName"))
-                registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                    if (!Settings.canDrawOverlays(this)) {
-                        // Permission not granted, handle accordingly
-                        checkPermission()
-                    } else {
-                        // Permission granted, handle accordingly
-                    }
-                }.launch(intent)
-            }
-        }
-    }
-
     private fun checkPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (!Settings.canDrawOverlays(this)) {
-                val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse("package:$packageName"))
-                startActivityForResult(intent, ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE)
+                val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName"))
+                overlayPermissionLauncher.launch(intent)
             }
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE) {
-            if (!Settings.canDrawOverlays(this)) {
-                // Permission not granted, handle accordingly
-                checkPermission()
-            } else {
-                // Permission granted, handle accordingly
+    private fun keepOverlayOnTop() {
+        val handler = Handler(Looper.getMainLooper())
+        val runnable = object : Runnable {
+            override fun run() {
+                windowManager.removeViewImmediate(overlayView)
+                windowManager.addView(overlayView, params)
+                handler.postDelayed(this, 1000)
             }
         }
+        handler.post(runnable)
+    }
+}
+
+class OverlayService : Service() {
+    private lateinit var windowManager: WindowManager
+    private lateinit var overlayView: LinearLayout
+    private lateinit var params: WindowManager.LayoutParams
+
+    override fun onCreate() {
+        super.onCreate()
+
+        windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        overlayView = LayoutInflater.from(this).inflate(R.layout.overlay_layout, null) as LinearLayout
+
+        val windowType = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+        } else {
+            WindowManager.LayoutParams.TYPE_SYSTEM_ALERT
+        }
+
+        params = WindowManager.LayoutParams(
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            windowType,
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or
+                    WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL or
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS or
+                    WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON or
+                    WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN or
+                    WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM or
+                    WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH or
+                    WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED or
+                    WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
+                    WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or
+                    WindowManager.LayoutParams.FLAG_SECURE or
+                    WindowManager.LayoutParams.FLAG_LAYOUT_ATTACHED_IN_DECOR or
+                    WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR,
+            PixelFormat.TRANSLUCENT
+        )
+        params.gravity = Gravity.TOP or Gravity.CENTER_HORIZONTAL
+
+        windowManager.addView(overlayView, params)
+        keepOverlayOnTop()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        keepOverlayOnTop()
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        windowManager.removeView(overlayView)
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    private fun keepOverlayOnTop() {
+        val handler = Handler(Looper.getMainLooper())
+        val runnable = object : Runnable {
+            override fun run() {
+                windowManager.removeViewImmediate(overlayView)
+                windowManager.addView(overlayView, params)
+                handler.postDelayed(this, 1000)
+            }
+        }
+        handler.post(runnable)
     }
 }
